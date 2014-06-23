efd3bba601014a26590e7ffac2ef123301cbaf8d
Johnson-wu <wu_yi_fan2013@163.com>
2014-06-23 15:55:53 +0800

    core python threading

---
+++ thread/method3_subclass.py
@@ -0,0 +1,46 @@
+#coding:utf-8
+
+import threading
+from time import ctime, sleep
+
+loops = (4,2)
+
+# 自定义线程，但是要重写__init__及run()方法
+class MyThread(threading.Thread):
+
+	def __init__(self, func, args, name=''):
+		threading.Thread.__init__(self)
+		self.func = func
+		self.args = args
+		self.name = name
+
+	def run(self):
+		self.func(*self.args)
+
+
+def loop(index, nsec):
+	print 'start loop', index, 'at:', ctime()
+	sleep(nsec)
+	print 'loop', index, 'done at:', ctime()
+
+def main():
+	print 'starting at:', ctime()
+	threads = []
+	nloops = range(len(loops))
+
+	for i in nloops:
+		t = MyThread(loop, (i, loops[i]), loop.__name__)
+		threads.append(t)
+
+	for i in nloops:
+		threads[i].start()
+
+	for i in nloops:
+		# use join() , the main thread will wait untill this thread exit
+		threads[i].join()
+
+	print 'All Done at :', ctime()
+
+
+if __name__ == '__main__':
+	main()
