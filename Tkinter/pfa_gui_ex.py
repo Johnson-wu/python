4c8ab8dfa13f1c977a31e798acd954d3babf0c0d
Johnson-wu <wu_yi_fan2013@163.com>
2014-06-29 16:56:19 +0800

    core python : Tkinter intermediate example

--- Tkinter/pfa_gui_ex.py
+++ Tkinter/pfa_gui_ex.py
@@ -39,9 +39,13 @@ for eachSign in SIGNS:
 	# 可以如此理解：%r 打印的是对象,是可以组成一个表达式的; %s打印的是可打印的字串内容
 	# 官方手册解释：The difference with repr(object) is that str(object) does not always attempt to return a string that is acceptable to eval(); 
 	# its goal is to return a printable string. If no argument is given, returns the empty string, ''.
+	# cmd = '%sButton(text=%r%s).pack(fill=Tkinter.X,expand=True)' % (
+	# 	signType.title(), eachSign,
+	# 	'.upper()' if signType == CRIT else '.title()')  # 直接插入条件表达式
+
 	cmd = '%sButton(text=%r%s).pack(fill=Tkinter.X,expand=True)' % (
 		signType.title(), eachSign,
-		'.upper()' if signType == CRIT else '.title()')  # 直接插入条件表达式
+	    signType == CRIT and '.upper()' or '.title()')
 	eval(cmd)
 
 root.mainloop()
