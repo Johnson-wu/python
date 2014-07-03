6412f013554b8f3664657aff67f7fc27551c5c19
Johnson-wu <wu_yi_fan2013@163.com>
2014-07-03 16:29:57 +0800

    挂号界面
    增加组件分组，使用labelframe

--- IDReader/gui.py
+++ IDReader/gui.py
@@ -5,73 +5,55 @@ import ttk
 from functools import partial
 
 WIDGETS_DICT = {
-	'IDCard Type': ['证件类型:','combobox',(0,0),(0,1)],
-	'IDCard Number': ['证件号码:','text',(0,2),(0,3)],
-	'Name': ['姓  名 :','text',(1,0),(1,1)],
-	'PinYin': ['首  拼 :','text',(1,2),(1,3)],
-	'Gender': ['性  别 :','combobox',(2,0),(2,1)],
-	'Brithday': ['出生日期:','text',(2,2),(2,3)],
-	'Address': ['地  址 :','text',(3,0),(3,1)],
-	'Phone': ['联系方式:','text',(3,2),(3,3)],
-	'Status': ['状  态 :','combobox',(4,0),(4,1)],
-	'Cost': ['挂号费:','combobox',(4,2),(4,3)],
-	'Reg Type': ['类  别 :','combobox',(5,0),(5,1)],
-	'Specialty': ['科  室 :','combobox',(5,2),(5,3)],
-	'Doctor': ['医  生 :','combobox',(6,0),(6,1)],
-	'Insurance Type': ['医保类型:','combobox',(6,2),(6,3)]
+	'IDCard Type': ['证件类型:','combobox',(0,0),(0,1),'cardInfo'],
+	'IDCard Number': ['证件号码:','text',(0,2),(0,3),'cardInfo'],
+	'Name': ['姓   名 :','text',(0,0),(0,1),'basicInfo'],
+	'PinYin': ['首   拼 :','text',(0,2),(0,3),'basicInfo'],
+	'Gender': ['性   别 :','combobox',(1,0),(1,1),'basicInfo'],
+	'Brithday': ['出生日期:','text',(1,2),(1,3),'basicInfo'],
+	'Address': ['地   址 :','text',(2,0),(2,1),'basicInfo'],
+	'Phone': ['联系方式:','text',(2,2),(2,3),'basicInfo'],
+	'Status': ['状   态 :','combobox',(0,0),(0,1),'medInfo'],
+	'Cost': ['挂号收费:','combobox',(0,2),(0,3),'medInfo'],
+	'Reg Type': ['挂号类型 :','combobox',(1,0),(1,1),'medInfo'],
+	'Specialty': ['挂号科室 :','combobox',(1,2),(1,3),'medInfo'],
+	'Doctor': ['医   生 :','combobox',(2,0),(2,1),'medInfo'],
+	'Insurance Type': ['医保类型:','combobox',(2,2),(2,3),'medInfo']
 }
 
 
 def register():
 	pass
-	
-
-def guiGeneratorByPack(master=None):
-	frames = []
-	frame01 = Tkinter.Frame(master, width=100, height=500, bg='green')
-	frames.append(frame01)
-	frame02 = Tkinter.Frame(master, width=200, height=500, bg='red')
-	frames.append(frame02)
-	frame03 = Tkinter.Frame(master, width=100, height=500, bg='green')
-	frames.append(frame03)
-	frame04 = Tkinter.Frame(master, width=200, height=500, bg='red')
-	frames.append(frame04)
-
-	frame05 = Tkinter.Frame(master, bg='yellow')
-	frame05.pack(side='bottom')
-
-	for frame in frames:
-		frame.pack(side=Tkinter.LEFT)
-
-	MyLabel01 = partial(Tkinter.Label, frame01)
-	cardType_lb = partial(MyLabel01, text='IDCard')
-	cardType_lb().pack(side='top')	# 注意cardType_lb()后面的()必须加上，不然报错AttributeError: 'functools.partial' object has no attribute 'pack'
-	idNumber_lb = partial(MyLabel01, text='IDNumber')
-	idNumber_lb().pack(side='top')
-
-	confirm_btn = Tkinter.Button(frame05, text='Confirm', command='')
-	quit_btn = Tkinter.Button(frame05, text='Cancel', command=master.quit)
-	quit_btn.pack(side=Tkinter.RIGHT,expand=1)
-	confirm_btn.pack(side=Tkinter.RIGHT,expand=1)
-	
 
 def guiGeneratorByGrid(master=None):
-	win = Tkinter.LabelFrame(master, text='Registration', height=400, width=500)
- 	win.grid_propagate(False)
- 	win.grid()
+	cardInfo = Tkinter.LabelFrame(master, text='Card Info', height=50, width=500)
+	basicInfo = Tkinter.LabelFrame(master, text='Basic Info', height=100, width=500)
+	medInfo = Tkinter.LabelFrame(master, text='Med Info', height=100, width=500)
+	btnFrame = Tkinter.Frame(master,height=50,width=500)
 
- 	MyLabel = partial(Tkinter.Label, win, height=1, width=10)
- 	MyText = partial(Tkinter.Text, win, height=1, width=20)
- 	MyCombox = partial(ttk.Combobox, win, height=1, width=23)
- 	MyButton = partial(Tkinter.Button, win, width=10)
+	frames = [cardInfo,basicInfo,medInfo,btnFrame]
+	for win in frames:
+	 	win.grid_propagate(False)
+	 	win.grid()
+
+ 	MyLabel = partial(Tkinter.Label, height=1, width=10)
+ 	MyText = partial(Tkinter.Text, height=1, width=20)
+ 	MyCombox = partial(ttk.Combobox, height=1, width=23)
+ 	MyButton = partial(Tkinter.Button, btnFrame, width=10)
 
  	for key in WIDGETS_DICT:
  		value = WIDGETS_DICT[key]
- 		label = partial(MyLabel, text=value[0].decode('gbk').encode('utf-8'))
+ 		if value[4] == 'cardInfo':
+ 			parent = cardInfo
+ 		elif value[4] == 'basicInfo':
+ 			parent = basicInfo
+ 		else:
+ 			parent = medInfo
+ 		label = partial(MyLabel, parent, text=value[0].decode('gbk').encode('utf-8'))
  		label().grid(row=value[2][0],column=value[2][1],sticky=Tkinter.W)
-
+  
  		if value[1] == 'text':
- 			text = partial(MyText)
+ 			text = partial(MyText, parent)
  			text().grid(row=value[3][0],column=value[3][1],sticky=Tkinter.W)
  		elif value[1] == 'combobox':
  			if key == 'IDCard Type':
@@ -90,13 +72,17 @@ def guiGeneratorByGrid(master=None):
  				var = ['Johnson','Jimmy','Tony']
  			elif key == 'Insurance Type':
  				var = ['Free','TotalPay']
- 			combo = partial(MyCombox)
- 			combo(value=var,justify='center').grid(row=value[3][0],column=value[3][1],sticky=Tkinter.W)
+ 			combo = partial(MyCombox, parent)
+ 			combo(value=var).grid(row=value[3][0],column=value[3][1],sticky=Tkinter.W)
 
- 	regBtn = partial(MyButton, command=register, text='确 认'.decode('gbk').encode('utf-8'))
- 	regBtn().grid(row=7,column=1)
- 	cancelBtn = partial(MyButton, command=win.quit, text='取 消'.decode('gbk').encode('utf-8'))
- 	cancelBtn().grid(row=7,column=3,sticky=Tkinter.W)
+ 	layLabel1 = partial(MyLabel,btnFrame,width=18)
+ 	layLabel2 = partial(MyLabel,btnFrame,width=15)
+ 	layLabel1().grid(row=0,column=0)
+ 	layLabel2().grid(row=0,column=2)
+ 	regBtn = partial(MyButton, command=register, text='确  定'.decode('gbk').encode('utf-8'))
+ 	regBtn().grid(row=0,column=1)
+ 	cancelBtn = partial(MyButton, command=win.quit, text='取  消'.decode('gbk').encode('utf-8'))
+ 	cancelBtn().grid(row=0,column=3)
 
  
 
@@ -104,7 +90,8 @@ def guiGeneratorByGrid(master=None):
 if __name__ == '__main__':
 	top = Tkinter.Tk()
 	top.title('挂号界面'.decode('gbk').encode('utf-8'))
-	top.geometry('500x230')
+	top.geometry('550x300')
+	# top['justify'] = 'center'
 	guiGeneratorByGrid(top)
 	top.mainloop()
 
