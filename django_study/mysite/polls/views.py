3012c5b2c9ae2c1fd890afe58d918fc057349eb6
Johnson-wu <wu_yi_fan2013@163.com>
2014-07-18 11:18:20 +0800

    core python : django
    replace :
    use shortcut : render() instead of HttpResponse
    add:
    add Http404

--- django_study/mysite/polls/views.py
+++ django_study/mysite/polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.http import HttpResponse, Http404
+# from django.template import RequestContext, loader
 
 from polls.models import *
 
@@ -9,14 +9,21 @@ def index(request):
 	# lastest_poll_orderby_date = Poll.objects.order_by('-pub_date')[:5]
 	lastest_poll_orderby_date = Poll.objects.order_by('-pub_date')
 	# output = ','.join([p.question for p in lastest_poll_orderby_date])
-	template = loader.get_template('polls/index.html')
-	context = RequestContext(request,
-			{'lastest_poll_orderby_date': lastest_poll_orderby_date},
-		)
-	return HttpResponse(template.render(context))
+	# template = loader.get_template('polls/index.html')
+	# context = RequestContext(request,
+	# 		{'lastest_poll_orderby_date': lastest_poll_orderby_date},
+	# 	)
+	context = {'lastest_poll_orderby_date': lastest_poll_orderby_date}
+	# return HttpResponse(template.render(context))
+	return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
-	return HttpResponse('You are looking at Poll %s.' % poll_id)
+	# return HttpResponse('You are looking at Poll %s.' % poll_id)
+	try:
+		poll = Poll.objects.get(pk=poll_id)
+	except Poll.DoesNotExist, e:
+		raise Http404
+	return render(request, 'polls/detail.html', {'poll': poll})
 
 def result(request, poll_id):
 	return HttpResponse('You are looking the result of Poll %s.' % poll_id)
